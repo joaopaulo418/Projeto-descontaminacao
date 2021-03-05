8eed4498402de838bf6a7285b0955958134d24c4
caioesqueletron <caiocaio620@gmail.com>
2021-03-04 22:35:59 -0300

    Adicionando mais features

--- Capiture.py
+++ Capiture.py
@@ -1,20 +1,24 @@
-import cv2
+import os
 import time
 from cv2 import cv2
 def main(args):
     aux = 0
     texto = ''
     nFrames = 30
+    diretorio = './Images'
+    for f in os.listdir(diretorio):
+        os.remove(os.path.join(diretorio,f))
     camera = cv2.VideoCapture(0)
     emLoop = True
     aux1 = 0
     aux = 0
     while (emLoop):
         tempo = 0
-        ini = time.time()
         retval, img = camera.read()
-        cv2.imshow('Foto', img)
-        file = 'Imagens/' + texto + '.png'
+        ini = time.time()
+        cv2.imshow('Camera', img)
+        file = './Images/imagenTeste'+texto+'.png'
+        cv2.waitKey(1)
         texto = str(aux1)
         aux1 = aux1 + 1
         cv2.imwrite(file, img)
@@ -22,9 +26,19 @@ def main(args):
         tempo = fim - ini
         aux = aux + tempo
         print (aux)
+        if aux1 == 288:
+            emLoop = False
+    
+
+    
+    
+    for f in os.listdir(diretorio):
+        os.remove(os.path.join(diretorio,f))
+
     cv2.destroyAllWindows()
     camera.release()
     return 0
+
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
