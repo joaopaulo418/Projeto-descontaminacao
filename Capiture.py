b710ea38f7a5425c0817f79c0eee5550ef99ebf1
caioesqueletron <caiocaio620@gmail.com>
2021-03-05 21:30:33 -0300

    updating files

--- Capiture.py
+++ Capiture.py
@@ -2,6 +2,13 @@ import os
 import time
 from cv2 import cv2
 
+def Binarization(src):
+    imgUMat = cv2.imread(src) #le o path atual da imagem
+    resized = cv2.resize(imgUMat, (64, 64), interpolation = cv2.INTER_CUBIC) # redimensiona a imagem
+    grayImage = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+
+    return grayImage
+
 def main(args):
     aux = 0
     texto = ''
@@ -12,20 +19,17 @@ def main(args):
     #camera.set(cv2.CAP_PROP_FRAME_WIDTH, 64)
     #camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 64) 
     emLoop = True
-    aux1 = 0
+    aux1 = 1
     aux = 0
     while (emLoop):
         tempo = 0
         _, img = camera.read()
         ini = time.time()
-        src = './Images/'+texto+'.png'
         texto = str(aux1) #transforma o numero em string
+        src = './Images/'+texto+'.png'
         cv2.imwrite(src, img)
-        imgUMat = cv2.imread(src) #le o path atual da imagem
-        resized = cv2.resize(imgUMat, (64, 64), interpolation = cv2.INTER_CUBIC) # redimensiona a imagem
-        grayImage = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
         aux1 = aux1 + 1
-        cv2.imwrite(src,grayImage) ##sobrescreverá a imagem com um novo formato
+        cv2.imwrite(src,Binarization(src)) ##sobrescreverá a imagem com um novo formato
         fim = time.time()
         tempo = fim - ini
         aux = aux + tempo
